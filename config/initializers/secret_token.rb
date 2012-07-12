5079f4b85d0f24d71887b5685bd7802c6b169a39
Sky Rumsey <sky@arttracker.co.uk>
2012-07-12 18:09:57 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinefilmrental::Application.config.secret_token = 'b3ce6d51a0007db9e9965d766637c558d3e3458b6c8d7b4ff941042ac6719fae1b229552098e62580a859832eadff6fedbde1610ee183e8e6283e6e0857b53dc'
