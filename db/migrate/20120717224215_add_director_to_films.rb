19a65632269826b9875c2436ef54aa3d70c7cba4
Sky Rumsey <sky@arttracker.co.uk>
2012-07-25 09:18:55 +0100

    removed migrations

--- db/migrate/20120717224215_add_director_to_films.rb
+++
@@ -1,5 +0,0 @@
-class AddDirectorToFilms < ActiveRecord::Migration
-  def change
-    add_column :films, :director, :string
-  end
-end
--- db/migrate/20120717224250_add_actors_to_films.rb
+++
@@ -1,5 +0,0 @@
-class AddActorsToFilms < ActiveRecord::Migration
-  def change
-    add_column :films, :actors, :string
-  end
-end
