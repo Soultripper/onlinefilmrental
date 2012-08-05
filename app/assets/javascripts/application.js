c5a9ab998f416fb97ab78de71360d460b8826bf1
Sky Rumsey <sky@arttracker.co.uk>
2012-08-05 21:27:46 +0100

    worked on film layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,12 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.ui.all
 //= require_tree .
 
 var films
 $(function()
 {
-
+    $('#tabs div').hide();
+$('#tabs div:first').show();
+$('#tabs ul li:first').addClass('active');
+ 
+$('#tabs ul li a').click(function(){
+$('#tabs ul li').removeClass('active');
+$(this).parent().addClass('active');
+var currentTab = $(this).attr('href');
+$('#tabs div').hide();
+$(currentTab).show();
+return false;
+});
 
 })
 
