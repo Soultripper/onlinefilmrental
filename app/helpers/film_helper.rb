65f328171eddd937e76e2dea2f92db28e78ac81c
Sky Rumsey <sky@arttracker.co.uk>
2012-08-05 22:48:54 +0100

    test disqus

--- app/helpers/film_helper.rb
+++ app/helpers/film_helper.rb
@@ -1,2 +1,11 @@
 module FilmHelper
+
+  def title_with_release_date
+    "#{@film.title} #{release_date}" 
+  end
+
+  def release_date
+    @film.release_date ? "(#{@film.release_date})" : nil 
+  end
+
 end
